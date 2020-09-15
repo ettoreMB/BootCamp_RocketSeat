df1b62f0e3ae1be2823b5e2590be31334ecad9d9
Ettore <ettore.barbosa@gmail.com>
2020-09-14 23:31:09 -0300

    instalacao nunjucks e  configuracoes de rotas

--- modulo_01/server.js
+++ modulo_01/server.js
@@ -1,8 +1,22 @@
 const  express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
 server.get('/', (req, res)=>{
-    return res.send("Hi, oi")
+    return res.render('index')
+})
+
+server.get('/classes', (req, res)=>{
+    return res.render('classes')
 })
 
 server.listen(5000, () => {
