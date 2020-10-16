fdaa53abc4478466d2e9471a35682c4f210114a6
ettore <ettore.barbosa@gmail.com>
2020-10-16 17:38:18 -0300

    Paginas de criacao aula 3 incompleta

--- SNLW/happy/web/src/pages/OrphanagesMap.tsx
+++ SNLW/happy/web/src/pages/OrphanagesMap.tsx
@@ -1,44 +1,83 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
-import "../styles/pages/orphanage.css"
-
+import "../styles/pages/orphanage-map.css"
+import api from '../services/api'
+import mapIcon from '../utils/mapIcon'
 import mapMarkerImg  from '../images/map_marker.svg'
-import {FiPlus} from 'react-icons/fi'
+import {FiPlus, FiArrowRight} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
-import {Map, TileLayer} from 'react-leaflet'
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
 
-import 'leaflet/dist/leaflet.css'
+
+
+interface Orphanage {
+   id: number,
+   latitude: number,
+   longitude: number,
+   name: string,
+}
+
 
 function OrphanagesMap() {
-    return (
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt=""/>
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([])
 
-                    <h2>Escolha um orfanato no mapa</h2>
-                    <p>Muitas crianças estão
-                    esperando a sua visita :)</p>
-                </header>
-                <footer>
-                    <strong>Sao Paulo</strong>
-                    <span>Sao Paulo</span>
-                </footer>
-            </aside>
+    useEffect(() => {
+        api.get('orphanages').then(response => {
+           setOrphanages(response.data)
+        })
+    }, [])
 
-            <Map
-                center={[-23.6286977,-46.5644216]}
-                zoom={15}
-                style={{width: '100%', height: '100%'}}
-            >
-                <TileLayer  url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-            </Map>
+    console.log(orphanages)
 
-            <Link to="" className="create-orphanage">
-                <FiPlus size={32} color="#FFF"/>
-            </Link>
-        </div>
+    return ( 
+    <div id="page-map">
+        <aside>
+            <header>
+                <img src={mapMarkerImg} alt=""/>
+
+                <h2>Escolha um orfanato no mapa</h2>
+                <p>Muitas crianças estão
+                esperando a sua visita :)</p>
+            </header>
+            <footer>
+                <strong>Sao Paulo</strong>
+                <span>Sao Paulo</span>
+            </footer>
+        </aside>
+
+        <Map
+            center={[-23.5649224,-46.5404211]}
+            zoom={15}
+            style={{width: '100%', height: '100%'}}
+        >
+
+            <TileLayer  url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+
+
+        {orphanages.map(orphanage => {
+            return (
+                <Marker
+                key={orphanage.id}
+                icon={mapIcon} 
+                position={[orphanage.latitude,orphanage.longitude]}
+                >
+                <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                {orphanage.name}
+                <Link to={`/orphanages/${orphanage.id}`}>
+                    <FiArrowRight size={20} color="#fff" />
+                </Link>
+                </Popup>
+                </Marker>
+            )
+        }) }  
+        </Map>
+
+        <Link to="/orphanages/create" className="create-orphanage">
+            <FiPlus size={32} color="#FFF"/>
+        </Link>
+    </div>
     )
 }
 
-export default OrphanagesMap
+export default OrphanagesMap;
---
+++ SNLW/happy/web/src/utils/mapIcon.ts
@@ -0,0 +1,12 @@
+import Leafleft from 'leaflet'
+import mapMarkerImg from '../images/map_marker.svg'
+import 'leaflet/dist/leaflet.css';
+
+const mapIcon = Leafleft.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [0, -60]
+  })
+
+  export default mapIcon
