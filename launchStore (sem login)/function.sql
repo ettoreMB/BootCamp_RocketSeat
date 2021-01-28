63109ef1afebea09e6b9897097a06b8d4b31f87c
EttoreMB <ettore.barbosa@gmail.com>
2021-01-28 14:36:54 -0300

    Dele launchstore sem login

--- launchStore (sem login)/function.sql
+++
@@ -1,7 +0,0 @@
-CREATE FUNCTION trigger_set_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at =NOW();
-  RETURN NEW;
-END;
-$$LANGUAGE plpgsql;
--- launchStore (sem login)/public/delete.js
+++
@@ -1,9 +0,0 @@
-const formDelete = document.querySelector('#form-delete')
-
-formDelete.addEventListener("submit", (event) => {
-    const confirmation = confirm('Deseja Deletar?') 
-        if(!confirmation) {
-            event.preventDefault
-        }
-    
-})
